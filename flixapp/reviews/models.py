cbe7eeee8791c6758fda9fc158dd7956039b3d1c
harshal mahapure <harshalmahapure44@gmail.com>
2023-03-22 03:16:39 +0900

    added user foreign key in review model to show one to many rel btw user and review

--- flixapp/reviews/models.py
+++ flixapp/reviews/models.py
@@ -1,11 +1,13 @@
 from films.models import WatchedMovie
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 class Review(models.Model):
     review = models.CharField(max_length=1000)
     movie = models.ForeignKey(WatchedMovie,on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+    review_by = models.ForeignKey(User,on_delete=models.CASCADE)
     
    
     
