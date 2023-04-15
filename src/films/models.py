1643e1a23365f2071b1a6370eca0b97c97841ec0
harshal <harshalmahapure44@gmail.com>
2023-04-15 11:53:43 +0530

    tried to change db schema

--- src/films/models.py
+++ src/films/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 
+ 
 class WatchedMovie(models.Model):
     tmdb_id = models.IntegerField(unique=True)
     original_title = models.CharField(max_length=100)
