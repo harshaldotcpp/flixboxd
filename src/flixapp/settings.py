cf12fd642e2140d68f6e6cce7d4ec51556f51b96
harshal <harshalmahapure44@gmail.com>
2023-04-15 18:07:45 +0530

    merged schema_changed branch

--- src/flixapp/settings.py
+++ src/flixapp/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'flixapp.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'flimflowSchema_change',
+        'NAME': 'flimflow',
         'USER': 'postgres',
         'PASSWORD': 'Flex@123',
         'HOST': 'localhost',
