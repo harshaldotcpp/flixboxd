b9f19beb9f9b9263cb16a5a9eb38d99d8e206f6b
harshal <harshalmahapure44@gmail.com>
2023-04-15 12:17:15 +0530

    seperate db for this branch

--- src/flixapp/settings.py
+++ src/flixapp/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'flixapp.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'flimflow',
+        'NAME': 'flimflowSchema_change',
         'USER': 'postgres',
         'PASSWORD': 'Flex@123',
         'HOST': 'localhost',
